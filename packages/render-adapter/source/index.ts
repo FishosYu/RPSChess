7cab94704cea4b332792aa2d23da1b6bc84866cd
Ximing Yu <155091240+FishosYu@users.noreply.github.com>
2026-07-03 11:42:26 +0800

    Delete packages directory

--- packages/render-adapter/source/index.ts
+++
@@ -1,4 +0,0 @@
-export { initPixiAppWithHighDpi } from './pixi.ts'
-export { installThreeHighDpi } from './three.ts'
-export type { PixiHighDpiOptions, PixiResizeMetrics } from './pixi.ts'
-export type { ThreeHighDpiOptions, ThreeResizeMetrics } from './three.ts'
